dc5c7d7474285b5440f356cc29d87902c0ca5d6c
zishan344 <zishanahmed344@gmail.com>
2022-05-01 12:28:51 +0600

    added jwt update next()e

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function verifyJWT(req, res, next) {
     }
     console.log("decoded", decoded);
     req.decoded = decoded;
+    next();
   });
-  next();
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iqmsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
