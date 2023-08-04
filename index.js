58190b5c2f89b8f62d03c3f2f4c0c1ae3a26ba2a
zishan344 <zishanahmed344@gmail.com>
2023-08-04 17:16:03 +0600

    database updated

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ function verifyJWT(req, res, next) {
   });
 }
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iqmsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iqmsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`; */
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iqmsj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
